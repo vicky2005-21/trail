ac4758bf69208786ac2bf9dee7f77ac6fd10c7f8
A.V.Vignesh Chowdary <74393849+vicky2005-21@users.noreply.github.com>
2025-03-28 16:10:44 +0530

    Delete constants directory

--- constants/relationship-interests.ts
+++
@@ -1,6 +0,0 @@
-export const RELATIONSHIP_INTENTS = [
-  { id: "friendship", name: "Friendship", emoji: "👋" },
-  { id: "dating", name: "Dating", emoji: "❤️" },
-  { id: "networking", name: "Networking", emoji: "💼" },
-  { id: "activity_partner", name: "Activity Partner", emoji: "🏃" },
-];
